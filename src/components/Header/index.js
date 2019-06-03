905019a4aacc465b02ddbb03437c175f345922ef
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-03 12:22:38 +0200

    Add content and styles to header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
-import React, {Fragment} from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
+import logoBmindHolidays from "../../images/logo-bmind-holidays.png";
 
-function Header(props){
-    return (
-        <header>
-                Header
-        </header>
-    )
+function Header(props) {
+  return (
+    <header className="Header">
+      <a href="https://www.bmind.es/es/" target="_blank">
+        <img src={logoBmindHolidays} alt="logo Bmind" className="Header__image" />
+      </a>
+      <p className="Header__wellcome">{`Hola, user1`}</p>
+    </header>
+  );
 }
 
 export default Header;
