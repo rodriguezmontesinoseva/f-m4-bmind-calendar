f92af0765dca908ea1714ec64013e30a4a0b51b0
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-03 12:34:18 +0200

    Add wellcome

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./styles.scss";
 import logoBmindHolidays from "../../images/logo-bmind-holidays.png";
+import PropTypes from "prop-types";
 
 function Header(props) {
+  const { selectedUser } = props;
   return (
     <header className="Header">
       <a href="https://www.bmind.es/es/" target="_blank">
-        <img src={logoBmindHolidays} alt="logo Bmind" className="Header__image" />
+        <img
+          src={logoBmindHolidays}
+          alt="logo Bmind"
+          className="Header__image"
+        />
       </a>
-      <p className="Header__wellcome">{`Hola, user1`}</p>
+      <p className="Header__wellcome">{`Hola, ${selectedUser.name}`}</p>
     </header>
   );
 }
 
+Header.propTypes = {
+    selectedUser: PropTypes.object.isRequired,
+  };
+
 export default Header;
+
