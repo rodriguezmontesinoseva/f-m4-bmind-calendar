b6c0c2e9b1250a6a7b42807e843ab9cf96fcf268
younglust <younglust@gmail.com>
2019-06-03 10:21:59 +0200

    add info user holidays

--- src/components/UserHoliday/index.js
+++ src/components/UserHoliday/index.js
@@ -1,12 +1,16 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import './styles.scss';
 
-function UserHoliday(props){
-    return (
-        <div>
-                info holiday
-        </div>
-    )
+function UserHoliday(props) {
+  return (
+    <div className='info-holidays'>
+      <ul className='info-holidays__days'>
+        <li>Vacaciones totales: 24</li>
+        <li>Vacaciones usadas: 12</li>
+        <li>Vacaciones disponibles: 12</li>
+      </ul>
+    </div>
+  );
 }
 
 export default UserHoliday;
