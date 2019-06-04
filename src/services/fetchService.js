80626db82ab1c351026217472beef80f66b7e228
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-04 11:38:34 +0200

    Change border colour

--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -1,5 +1,5 @@
 function fetchService(){
-    const url = 'https://adalab.bmind.es/api/users';
+    const url = 'https://raw.githubusercontent.com/Adalab/f-m4-bmind-calendar/master/src/data/users.json';
     return (    fetch(url)
       .then(response => 
           response.json())
