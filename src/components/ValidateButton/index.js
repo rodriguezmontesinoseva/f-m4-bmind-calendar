43f7bc6402421443fa951f4cd1209faff0d23401
Elvira Fuente <hebebicho@gmail.com>
2019-06-04 17:19:20 +0200

    add validation text

--- src/components/ValidateButton/index.js
+++ src/components/ValidateButton/index.js
@@ -8,10 +8,10 @@ const ValidateButton = props => {
         <div className="Main__legend-color-approved" />
         <p>Vacaciones aprobadas</p>
         <div className="Main__legend-color-requested" />
-        <p>Vacaciones solicitadas</p>
+        <p>Vacaciones seleccionadas</p>
       </div>
       <div className="info__container__info-button">
-      <p className="info__paragraph">Delivered information</p>
+      <p className="info__paragraph">{props.isDisabled?'Si quieres elegir tus vacaciones, vuelve a tu usuario':''}</p>
       <button
         className="btn-validate"
         disabled={props.isDisabled}
